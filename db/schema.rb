87c05b6794dc67374b68a7142301d87e125f8f1c
syrissel <sdbmireault@gmail.com>
2019-11-13 00:37:15 -0600

    Revised more styles. Added toc plugin to tinymce

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_010058) do
+ActiveRecord::Schema.define(version: 2019_11_13_060752) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "url"
