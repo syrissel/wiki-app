ff0c1ed2abc7ad670a49a37d3b1ce34a36e361dd
syrissel <sdbmireault@gmail.com>
2020-01-24 22:43:31 -0600

    updated user registration workflow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @active = UserStatus.find_by_status('Active').id
   end
 
   def create
@@ -33,6 +34,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation, :user_level_id, :first_name, :last_name)
+    params.require(:user).permit(:username, :password, :password_confirmation, :user_level_id, :first_name, :last_name, :user_status_id)
   end
 end
