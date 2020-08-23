9a7d1cf94da98c941fc822e9fa4ef263d8f61d4b
syrissel <sdbmireault@gmail.com>
2020-08-23 01:01:29 -0500

    Add animations

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,9 +1,15 @@
 $(document).on('turbolinks:load', function () {
     $('.admin-item-wrapper').hover(function () {
         $(this).children('.icon').attr('animation', 'burst')
+        $(this).children('p').animate({
+            'font-size': '30px'
+        }, 250)
         $(this).parent().parent().addClass('custom-shadow')
     }, function () {
         $(this).children('.icon').removeAttr('animation')
+        $(this).children('p').animate({
+            'font-size': '1rem'
+        }, 250)
         $(this).parent().parent().removeClass('custom-shadow')
     })
 })
