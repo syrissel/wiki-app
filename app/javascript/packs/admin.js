ff692e5dfb489b39202efad305ee30002ff4d669
syrissel <sdbmireault@gmail.com>
2020-08-23 15:46:42 -0500

    Update admin portal styles.

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,18 +1,18 @@
-$(document).on('turbolinks:load', function () {
+$(document).ready(function () {
     $('.admin-item-wrapper').hover(function () {
         if ($(this).children('.icon').attr('name') == 'cog') {
             $(this).children('.icon').attr('animation', 'spin')
         } else {
             $(this).children('.icon').attr('animation', 'tada')
         }
-        $(this).children('p').animate({
+        $(this).children('h4').animate({
             'font-size': '30px'
         }, 250)
         $(this).parent().parent().addClass('custom-shadow')
     }, function () {
         $(this).children('.icon').removeAttr('animation')
-        $(this).children('p').animate({
-            'font-size': '1rem'
+        $(this).children('h4').animate({
+            'font-size': '1.5rem'
         }, 250)
         $(this).parent().parent().removeClass('custom-shadow')
     })
