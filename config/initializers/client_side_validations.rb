1b8faba4ee29b0bcb8ae6207d8ce4ccf73966253
syrissel <sdbmireault@gmail.com>
2020-08-23 15:58:22 -0500

    Update change password styles

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -16,6 +16,6 @@ ActionView::Base.field_error_proc = proc do |html_tag, instance|
   if html_tag =~ /^<label/
     %(<div class="field_with_errors">#{html_tag}</div>).html_safe
   else
-    %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}" style="display:block;" class="message errors">#{instance.error_message.first}</label></div>).html_safe
+    %(<div class="field_with_errors">#{html_tag}<label for="#{instance.send(:tag_id)}"class="message text-danger">#{instance.error_message.first}</label></div>).html_safe
   end
 end
