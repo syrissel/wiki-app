74963bfdf8af33560b5751d21c5bc9198056d0c9
syrissel <sdbmireault@gmail.com>
2020-08-24 12:34:42 -0500

    Add image and video functionality.

--- app/javascript/packs/summernote.js
+++ app/javascript/packs/summernote.js
@@ -1,5 +1,46 @@
 require('bootstrap')
 require('summernote/dist/summernote-bs4')
+require('custom/summernote-table-of-contents')
+
+let ImageButton = function (context) {
+    let ui = $.summernote.ui;
+
+    let button = ui.button({
+        contents: '<i class="fa fa-camera" aria-hidden="true"></i>',
+        tooltip: 'Image',
+        click: function () {
+            let options = document.getElementsByClassName('image_options')[0];
+
+            if (options.classList.contains('hidden')) {
+                options.className = options.className.replace("hidden", "");
+            } else {
+                options.classList.add("hidden");
+            }
+        }
+    })
+
+    return button.render()
+}
+
+let VideoButton = function (context) {
+    let ui = $.summernote.ui;
+
+    let button = ui.button({
+        contents: '<i class="fa fa-video-camera" aria-hidden="true"></i>',
+        tooltip: 'Video',
+        click: function () {
+            let options = document.getElementsByClassName('video_options')[0];
+        
+            if (options.classList.contains('hidden')) {
+                options.className = options.className.replace("hidden", "");
+            } else {
+                options.classList.add("hidden");
+            }
+        }
+    })
+
+    return button.render()
+}
 
 $(document).ready(function() {
     $('#summernote').summernote({
@@ -10,14 +51,15 @@ $(document).ready(function() {
             ['color', ['color']],
             ['para', ['ul', 'ol', 'paragraph']],
             ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
+            ['insert', ['link', 'btnImage', 'btnVideo']],
             ['view', ['fullscreen', 'codeview', 'help']],
-            ['btnHello', ['hello']]
+            ['tableofcontent']
           ],
         buttons: {
-            hello: helloButton
+            btnImage: ImageButton,
+            btnVideo: VideoButton
         },
-        tabDisable: false
+        tabDisable: true
     });
 
     var helloButton = function (context) {
@@ -36,17 +78,6 @@ $(document).ready(function() {
         return button.render();   // return button as jquery object
       }
 
-    let ImageButton = function (context) {
-        let ui = $.summernote.ui;
-
-        let button = ui.button({
-            contents: '<p>Text</p>',
-            tooltip: 'Add a video'
-        })
-
-        return button.render()
-    }
-
     $('#image_button').click(function() {
         let url = $("input[name='image']:checked").data('image').url;
 
@@ -59,6 +90,8 @@ $(document).ready(function() {
         let url = $("input[name='video']:checked").data('video').url;
         let thumbnail = $("input[name='video']:checked").data('thumbnail');
         let video = document.createElement('video')
+        let p = document.createElement('p')
+        let br = document.createElement('br')
         video.setAttribute('src', url)
         video.setAttribute('allowfullscreen', true)
         video.setAttribute('frameborder', '0')
@@ -66,6 +99,11 @@ $(document).ready(function() {
         video.setAttribute('poster', thumbnail)
         video.setAttribute('class', 'video-player')
 
+        let range = $.summernote.range
+
         $('#summernote').summernote('insertNode', video)
+        $('#summernote').summernote('insertNode', br)
+        // $('#summernote').summernote('editor.setLastRange', range.createFromNodeAfter(video).select())
+        console.log($('#summernote').summernote('editor.getLastRange'))
     })
 })
