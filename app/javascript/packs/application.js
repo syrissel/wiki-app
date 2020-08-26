82d798358c6348e423cc93ddb3b4eb2f155b0786
syrissel <sdbmireault@gmail.com>
2020-08-25 19:42:07 -0500

    Fix notification alerts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,7 +22,7 @@ import 'custom/wiki-validate';
 
 $(document).on('turbolinks:load', function () {
     $('#notification_bell').click(function () {
-        if ($('#notifications').css('display') === 'block') {
+        if ($('#notifications').css('display') == 'block') {
             $('#notifications').hide()
         } else {
             $('#notifications').show()
