08411b9d4377d96f1d15bc42880e40f5c8c8a395
syrissel <sdbmireault@gmail.com>
2020-09-16 21:39:19 -0500

    Add category modal

--- app/javascript/custom/search.js
+++ app/javascript/custom/search.js
@@ -1,3 +1,5 @@
+require('bootstrap')
+
 $(document).on('turbolinks:load', function() {
     $('#btn_filter_options').click(function() {
 
@@ -6,6 +8,13 @@ $(document).on('turbolinks:load', function() {
         } else {
             $('#filter_options').addClass('d-none')
         }
+    })
+
+    $('#btn_other_search_options').click(function() {
+
+        $('.form-check-input').prop('checked', false)
+        // $('.form-check-input').attr('disabled', 'disabled')
+        $('#modal_category_select').modal('show')
     })
 })
 
