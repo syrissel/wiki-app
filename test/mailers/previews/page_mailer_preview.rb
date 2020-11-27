a534ec8605b57f63e69033ad1565dee231640d46
syrissel <sdbmireault@gmail.com>
2020-11-27 09:56:22 -0600

    Adjust link

--- test/mailers/previews/page_mailer_preview.rb
+++ test/mailers/previews/page_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/page_mailer
 class PageMailerPreview < ActionMailer::Preview
   def new_page_email
-    page = Page.new(title: 'Mailer Test', content: "Here's some content.", category_id: 9, user_id: 1)
+    page = Page.new(title: 'Mailer Test', content: "Here's some content.", category_id: 9, user_id: 1, approval_status_id: 1)
 
     PageMailer.with(page: page).new_page_email
   end
