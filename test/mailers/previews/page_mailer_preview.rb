44503dc569dfda1a8e83cee4671e5fcdbf5c7290
syrissel <sdbmireault@gmail.com>
2020-11-30 08:18:58 -0600

    Fix executive mailer

--- test/mailers/previews/page_mailer_preview.rb
+++ test/mailers/previews/page_mailer_preview.rb
@@ -5,4 +5,10 @@ class PageMailerPreview < ActionMailer::Preview
 
     PageMailer.with(page: page).new_page_email
   end
+
+  def new_executive_email
+    page = Page.new(title: 'Mailer Test', content: "Here's some content.", category_id: 9, user_id: 1, approval_status_id: 1, approved_by_id: 4)
+
+    PageMailer.with(page: page).new_executive_email
+  end
 end
