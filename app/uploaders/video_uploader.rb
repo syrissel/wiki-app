02bce97a428521623d86153fa7e29f9fee4fc347
syrissel <sdbmireault@gmail.com>
2020-12-04 10:02:36 -0600

    Upload different video formats. When uploaded will be mp4

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -39,7 +39,7 @@ class VideoUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(mp4)
+    %w(mp4 mov avi wmv flv)
   end
 
   # Override the filename of the uploaded files:
