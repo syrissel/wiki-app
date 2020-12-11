3d4ad758c1f5a1f6a7e447148fbfd1af189cedea
syrissel <sdbmireault@gmail.com>
2020-12-11 12:42:56 -0600

    Add delivery options

--- app/mailers/page_mailer.rb
+++ app/mailers/page_mailer.rb
@@ -1,24 +1,24 @@
 class PageMailer < ApplicationMailer
   def new_page_email
     @page = params[:page]
-    @settings = Setting.first
+    @settings = params[:settings]
     delivery_options = { user_name: @settings.smtp_username,
                          password:  @settings.smtp_password,
                          address:   @settings.smtp_address,
                          port:      @settings.smtp_port }
 
     # mail(to: ENV["admin_email"], subject: "New wiki submitted by #{@page.user.fullname}")
-    mail(to: User.only_supervisors.where("email IS NOT NULL").where(user_status_id: 1).where.not(id: @page.user_id).pluck(:email), subject: "New wiki submitted by #{@page.user.fullname}")
+    mail(from: @settings.smtp_username, to: User.only_supervisors.where("email IS NOT NULL").where(user_status_id: 1).where.not(id: @page.user_id).pluck(:email), subject: "New wiki submitted by #{@page.user.fullname}")
   end
 
   def new_executive_email
     @page = params[:page]
-    @settings = Setting.first
+    @settings = params[:settings]
     delivery_options = { user_name: @settings.smtp_username,
                          password:  @settings.smtp_password,
                          address:   @settings.smtp_address,
                          port:      @settings.smtp_port }
-                         
-    mail(to: User.executives.where("email IS NOT NULL").where(user_status_id: 1).where.not(id: @page.user_id).pluck(:email), subject: "New wiki submitted by #{@page.user.fullname}")
+
+    mail(from: @settings.smtp_username, to: User.executives.where("email IS NOT NULL").where(user_status_id: 1).where.not(id: @page.user_id).pluck(:email), subject: "New wiki submitted by #{@page.user.fullname}")
   end
 end
